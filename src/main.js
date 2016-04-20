bf328bb29ac2ca14a2b1b21472d78f453362c7ce
David Nunez <arizonatribe@gmail.com>
2016-04-20 16:04:36 -0700

    Implemented user add feature

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 var ldap = require('ldapjs'),
     fs = require('fs'),
+    spawn = require('child_process').spawn,
     server = ldap.createServer();
 
 server.listen(1389, () => console.log(`LDAP server listening at ${server.url}`));
@@ -21,6 +22,49 @@ server.search('o=myhost', [authorize, loadPasswdFile], (req, res, next) => {
   res.end();
   return next();
 });
+
+server.add('ou=users, o=myhost', [authorize, loadPasswdFile], (req, res, next) => {
+  var entry = req.toObject().attributes;
+
+  if (!entry.cn || !entry.cn[0])
+    return next(new ldap.ConstraintViolationError('cn required'));
+
+  if (req.users[entry.cn[0]])
+    return next(new ldap.EntryAlreadyExistsError(req.dn.toString()));
+
+  if (!entry.objectclass || !/unixUser/.test(entry.objectclass[0]))
+    return next(new ldap.ConstraintViolation('entry must be a unixUser'));
+
+  let useradd = spawn('useradd', ldapUserDetailsToOptions(entry)),
+      messages = [];
+
+  useradd.stdout.on('data', data => messages.push(data.toString()));
+  useradd.stderr.on('data', data => messages.push(data.toString()));
+  useradd.on('exit', code => {
+    if (code !== 0) {
+      return next(new ldap.OperationsError(`${code}${messages.length ? `:${messages.join()}` : ''}`));
+    }
+
+    res.end();
+    return next();
+  });
+});
+
+function ldapUserDetailsToOptions(user) {
+  let opts = ['-m'];
+
+  (new Map([
+    ['-c', user.description], 
+    ['-d', user.homedirectory],
+    ['-g', user.gid],
+    ['-s', user.shell],
+    ['-u', user.uid]
+  ]).forEach((val, key) => val && opts.push(key, val[0])));
+
+  opts.push(user.cn[0]);
+
+  return opts; 
+}
 
 function authorize(req, res, next) {
   if (!req.connection.ldap.bindDN.equals('cn=root'))
